1605bbef5421bd1e75abefad097f34360dbbbe10
robinmartis <robin.martis@gmail.com>
2025-07-22 23:27:44 +0530

    Added allure report

--- CucumberSeleniumFramework/src/test/java/runners/TestRunner.java
+++ CucumberSeleniumFramework/src/test/java/runners/TestRunner.java
@@ -7,6 +7,7 @@ import io.cucumber.testng.CucumberOptions;
     features = "src/test/java/features",
     glue = {"stepdefinitions", "hooks"},
     plugin = {"pretty", 
+    		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm", // Allure plugin
     		"html:target/cucumber-reports.html", 
     		"json:target/cucumber.json", 
     		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"  // Add Extent adapter
